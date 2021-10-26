5b2ba031f3825fd72ee0bc5b9fa4a0aaae6f01b5
DeVontae316 <DeVontaeMoore316@yahoo.com>
2021-10-26 15:45:48 -0500

    Added Gatsby's new Image component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { LayoutWrapper } from "./style";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <LayoutWrapper>
+        <main>{children}</main>
+      </LayoutWrapper>
+    </>
+  );
+};
+
+export { Layout };
--- src/templates/Product/index.js
+++ src/templates/Product/index.js
@@ -1,21 +1,42 @@
 import React from "react";
+import Image from "components/Image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
 import { graphql } from "gatsby";
+import { Layout } from "components";
+import { Grid } from "./style";
 
 export const pageQuery = graphql`
   query pageQuery($shopifyId: String!) {
     shopifyProduct(shopifyId: { eq: $shopifyId }) {
       description
+      images {
+        gatsbyImageData(width: 300)
+      }
     }
   }
 `;
 
 const ProductTemplate = ({ data }) => {
   console.log(data);
+  console.log("Img below:");
+  console.log(data.shopifyProduct.images[0]);
   return (
-    <div>
-      <h1>product template</h1>
-      {data.shopifyProduct.description}
-    </div>
+    <Layout>
+      <Grid>
+        <div>
+          <h1>product template</h1>
+          {data.shopifyProduct.description}
+        </div>
+        <div>
+          <h1>Image here</h1>
+          <GatsbyImage
+            image={data.shopifyProduct.images[0].gatsbyImageData}
+            alt=""
+          />
+        </div>
+      </Grid>
+    </Layout>
   );
 };
 
